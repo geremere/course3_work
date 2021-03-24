1aa84332ab07ec584ba4220fc9cfb6a78a856430
student <rmimamov@edu.hse.ru>
2021-03-24 14:38:39 +0300

    fix 2

---
+++ polling-app-client/src/components/user/chat/Messenger.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import style from "./Messenger.module.css"
+import "./chat.css"
+
+class Messenger extends Component {
+        // constructor(props) {
+    //     super(props)
+    //     this.state = {}
+    // }
+
+    render() {
+        console.log(style.messenger_box)
+        return (
+            <div>
+                <div className={style.boxq}>
+                    вдпдлдлывплдрвдлп
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Messenger;
